c084d959f40ecb9aacceb1d8e8752de941932fae
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-03 21:28:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,7 @@ end
 #def take_a_number(katz_deli, name)
   katz_deli.push(name)
   customer = katz_deli[-1]
-  customer.to_s
   num = katz_deli.length + 1
-  num.to_s
+  num = num.to_s
   puts "Welcome, " + customer + ". You are number " + num + " in line."
 #end
