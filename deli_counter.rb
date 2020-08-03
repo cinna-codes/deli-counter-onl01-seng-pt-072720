fd4b6c0e1b1c5cb941a700497edba4d318620a77
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-03 19:22:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length > 0
+    line = katz_deli.each_with_index.map do |name, i|
+      "#{i+1}. #{name}"
+    end
+    line = line.join(" ")
+    puts "The line is currently: " + line
+  else katz_deli.length == 0
+      puts "The line is currently empty."
+end
+  end
+
+#def take_a_number(array, name)
+
+#end
